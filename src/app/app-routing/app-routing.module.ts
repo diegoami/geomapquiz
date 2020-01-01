b4e8c6769d76bc6b9ed0fa44a35fa84eda32adfe
diegoami <diego.amicabile@gmail.com>
2020-01-01 15:58:33 +0100

    Add Car dashboard

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {CarPageComponent} from '../car-page/car-page.component';
+import {CarDashboardComponent} from '../car-dashboard/car-dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CarPageComponent,
   },
+  {
+    path: 'dashboard',
+    component: CarDashboardComponent,
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,14 @@ import { DialogModule } from 'primeng/dialog';
 import { AppComponent } from './app.component';
 import { CarPageComponent } from './car-page/car-page.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import { CarDashboardComponent } from './car-dashboard/car-dashboard.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        CarPageComponent
+        CarPageComponent,
+        CarDashboardComponent
     ],
     imports: [
         BrowserModule,
